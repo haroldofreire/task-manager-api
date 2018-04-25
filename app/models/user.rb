21723995fc3d37b89ba5e9f7b295f3b8cebfb8a0
Haroldo Freire <contato@haroldofreire.com.br>
2018-04-25 04:22:21 +0000

    Updating User#info

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   validates_uniqueness_of :auth_token
   def info
-    "#{email} - #{created_at}"
+    "#{email} - #{created_at} - Token: #{Devise.friendly_token}"
   end
 end
