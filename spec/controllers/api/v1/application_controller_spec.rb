b3f262579ba0f651b53ef871e4c9ccf5fec5eb1a
Haroldo Freire <contato@haroldofreire.com.br>
2018-04-27 22:33:45 +0000

    Adding authenticable concern with #current_user

--- spec/controllers/api/v1/application_controller_spec.rb
+++ spec/controllers/api/v1/application_controller_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::ApplicationController, type: :controller do
   describe "include the corrects concerns" do
-    it { expect{controller.class.ancestors}.to include(Authenticable) } 
+    it { expect(controller.class.ancestors).to include(Authenticable) } 
   end
 end
