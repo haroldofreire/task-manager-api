85e1342991446a27039c51fece986c38b0cf64ab
Haroldo Freire <contato@haroldofreire.com.br>
2018-04-29 01:32:15 +0000

    Adding authorization for the users controller

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe 'Users API', type: :request do
   let(:headers) do
     {
       'Accept' => 'application/vnd.taskmanager.v1',
-      'Content-Type' => Mime[:json].to_s
+      'Content-Type' => Mime[:json].to_s,
+      'Authorization' => user.auth_token
     }
   end
 
@@ -99,8 +100,9 @@ RSpec.describe 'Users API', type: :request do
 
 describe 'DELETE /users/:id' do
   before do
-    delete "/users/#{user_id}", params: {}, headers: {} 
+    delete "/users/#{user_id}", params: {}, headers: headers
   end
+
   it 'returns status code 204' do
     expect(response).to   have_http_status(204)
   end
