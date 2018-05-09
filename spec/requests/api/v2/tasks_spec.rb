a05c0036a437a8026da9b0c2196643d491bbbd40
Haroldo Freire <contato@haroldofreire.com.br>
2018-05-09 03:17:21 +0000

    erro nos testes - GET /tasks - apos active_model_serializers

--- spec/requests/api/v2/tasks_spec.rb
+++ spec/requests/api/v2/tasks_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe 'Task API' do
   let!(:user) { create(:user)}
   let(:headers) do
     {
-      'Accept' => 'application/vnd.taskmanager.v2',
       'Content-Type' => Mime[:json].to_s,
+      'Accept' => 'application/vnd.taskmanager.v2',
       'Authorization' => user.auth_token
     }
   end
 
-  describe "GET /task" do
+  describe "GET /tasks" do
     before do
       create_list(:task, 5, user_id: user.id)
       get '/tasks', params: {}, headers: headers
@@ -23,7 +23,7 @@ RSpec.describe 'Task API' do
     end
 
     it 'returns 5 tasks from database' do
-      expect(json_body[:tasks].count).to eq(5)
+      expect(json_body[:data].count).to eq(5)
     end
   end
 
