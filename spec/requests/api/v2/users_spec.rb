2dd9fc56dc57e6203460a808fb567ad7a70e254a
Haroldo Freire <contato@haroldofreire.com.br>
2018-06-30 23:06:04 +0000

    update the users api

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -2,25 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
-  let(:user_id) { user.id }
+  let!(:auth_data) { user.create_new_auth_token }
   let(:headers) do
     {
       'Accept' => 'application/vnd.taskmanager.v2',
       'Content-Type' => Mime[:json].to_s,
-      'Authorization' => user.auth_token
+      'access-token' => auth_data['access-token'],
+      'uid' => auth_data['uid'],
+      'client'=> auth_data['client']
     }
   end
 
   before { host! 'api.taskmanager.test'}
 
-  describe 'GET /users/:id' do
-    before do
-      get "/users/#{user_id}", params: {}, headers: headers
-    end
+  describe 'GET /auth/validate_token' do
+    
+    context 'when the request headers are valid' do
+      before do
+        get '/auth/validate_token', params: {}, headers: headers
+      end
 
-    context 'when the user exists' do
       it 'returns the user' do
-        expect(json_body[:data][:id].to_i).to eq(user_id)
+        expect(json_body[:data][:id].to_i).to eq(user.id)
       end
        
       it 'returns status 200' do
@@ -28,30 +31,33 @@ RSpec.describe 'Users API', type: :request do
       end
     end
     
-    context 'when the user does not exists' do
-      let(:user_id) { 10000 }
-      it 'returns status 404' do
-        expect(response).to have_http_status(404)
+    context 'when the request headers are not valid' do
+      before do
+        headers['access-token'] = 'invalid-token'
+        get '/auth/validate_token', params: {}, headers: headers
+      end
+      it 'returns status 401' do
+        expect(response).to have_http_status(401)
       end
     end
   end #fim do describe 'GET /users/:id'
 
 
-  describe 'POST /users' do
+  describe 'POST /auth' do
     
     before do
-      post '/users', params: { user: user_params }.to_json, headers: headers
+      post '/auth', params: user_params.to_json, headers: headers
     end
     
     context 'when the request params are valid' do
       let(:user_params) { attributes_for(:user) }
 
-      it 'return status code 201' do
-        expect(response).to have_http_status(201)
+      it 'return status code 200' do
+        expect(response).to have_http_status(200)
       end
 
       it 'return jason data for the created user' do
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end
 
     end #fim do context 'when the request params are valid'
@@ -69,9 +75,9 @@ RSpec.describe 'Users API', type: :request do
     end #fim do context 'when the request params are invalid'
   end #fim do describ 'POST /users'
 
-  describe 'PUT /users/:id' do
+  describe 'PUT /auth' do
     before do
-      put "/users/#{user_id}", params: {user: user_params}.to_json, headers: headers
+      put '/auth', params: user_params.to_json, headers: headers
     end
 
     context 'when the request params are valid' do
@@ -81,7 +87,7 @@ RSpec.describe 'Users API', type: :request do
       end
       it 'returns the json data for the update user' do
         user_reponse = JSON.parse(response.body, symbolize_names: true)
-        expect(user_reponse[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(user_reponse[:data][:email]).to eq(user_params[:email])
       end
     end
     
@@ -98,13 +104,13 @@ RSpec.describe 'Users API', type: :request do
     end #fim do context 'when the request params are invalid'
   end #fim do describe 'PUT /users/:id'
 
-describe 'DELETE /users/:id' do
+describe 'DELETE /auth' do
   before do
-    delete "/users/#{user_id}", params: {}, headers: headers
+    delete '/auth', params: {}, headers: headers
   end
 
-  it 'returns status code 204' do
-    expect(response).to   have_http_status(204)
+  it 'returns status code 200' do
+    expect(response).to   have_http_status(200)
   end
 
   it 'remove from users database' do
