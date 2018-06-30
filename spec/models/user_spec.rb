04dead00bf76739c5bfa3d74b712d70fa6fc9ae8
Haroldo Freire <contato@haroldofreire.com.br>
2018-06-30 02:13:20 +0000

    installing devise_token_auth

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
 
-  it{ is_expected.to have_many(:tasks).dependent(:destroy) }
+  it{  is_expected.to have_many(:tasks).dependent(:destroy) }
 
-  it { expect(user).to validate_presence_of(:email) }  
-  it { expect(user).to validate_uniqueness_of(:email).case_insensitive }  
-  it { expect(user).to validate_confirmation_of(:password) }  
-  it { expect(user).to allow_value('contato@haroldofreire.com.br').for(:email) }  
-  it { expect(user).to validate_uniqueness_of(:auth_token) }  
+  it { is_expected.to validate_presence_of(:email) }  
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }  
+  it { is_expected.to validate_confirmation_of(:password) }  
+  it { is_expected.to allow_value('costa@nonato.com').for(:email) }  
+  it { is_expected.to validate_uniqueness_of(:auth_token) }  
 
   describe '#info' do
-    it 'returns email, created_at and token' do
+    it 'returns email, created_at and a Token' do
       user.save!
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
 
@@ -20,7 +20,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "#generate_authentication_token!" do
+  describe '#generate_authentication_token!' do
     it 'generates a unique auth token' do
       allow(Devise).to receive(:friendly_token).and_return('abc123xyzTOKEN')
       user.generate_authentication_token!
@@ -28,8 +28,8 @@ RSpec.describe User, type: :model do
       expect(user.auth_token).to eq('abc123xyzTOKEN')  
     end
 
-    it 'generates another auth token whe the current auth token already has been token' do
-      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz','abc123tokenxyz', 'abcXYZ123456789')
+    it 'generates another auth token when the current auth token already has been token' do
+      allow(Devise).to receive(:friendly_token).and_return('abc123tokenxyz', 'abc123tokenxyz', 'abcXYZ123456789')
       existing_user = create(:user)
       user.generate_authentication_token!
 
